7183fae0f21e6ed204c4fab4d1e50f5a2197b1b4
ThiagoVilalva1905 <thiago_vilalva@ufms.br>
2024-11-13 20:38:23 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+// service-worker.js
+
+// Evento de instalação
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('my-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/Quiz.html',
+          // Adicione outros arquivos que deseja armazenar em cache
+            ]);
+        })
+    );
+});
+  
+  // Evento de ativação
+  self.addEventListener('activate', (event) => {
+    // Limpe caches antigos, se necessário
+  });
+  
+  // Intercepte as requisições e retorne do cache, se disponível
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+  
